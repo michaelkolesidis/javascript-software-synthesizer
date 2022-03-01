ea636460eedf79bec33c37bcc664729131867730
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-03-02 01:26:03 +0200

    added keyboard styling options

--- scripts/header.js
+++ scripts/header.js
@@ -13,5 +13,3 @@ export default function header(darkMode) {
   return `<div id="logo-name"><img src="./assets/logo/logo192.png"/>
     <div>JSS-01 — JavaScript Software Synthesizer</div></div><div id="buttons"><button type="button" id="dark">🌓︎</button><button type="button" id="help">?</button><button id="menu" type="button">☰</button></div>`;
 }
-
-
