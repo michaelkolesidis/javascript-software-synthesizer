42bd2939d0313f9070bdcf66159246bceae4b808
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-03-15 00:51:48 +0200

    added show/hide function intro

--- scripts/functions/showHide.js
+++ scripts/functions/showHide.js
@@ -1,4 +1,12 @@
-
+/*
+ * Show/Hide
+ * Part of the JSS-01 | JavaScript Software Synthesizer project
+ * Copyright (c) 2022 Michael Kolesidis
+ * MIT License
+ *
+ * The Show/Hide function allows the user to hide and show a
+ * section by clicking on its title.
+ */
 
 export default function showHide(title, section, display) {
   // (title, section)
