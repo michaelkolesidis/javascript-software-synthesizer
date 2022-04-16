b700ec2757f7de8d848ae32d48a07bcacc53aeb4
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-04-16 13:09:03 +0300

    console.log styles in const

--- scripts/functions/consoleIntro.js
+++ scripts/functions/consoleIntro.js
@@ -8,11 +8,14 @@
  * in the console upon the start of the app.
  */
 
+const styles = [
+  "background: rgb(1, 0, 76)",
+  "color: rgb(230, 230, 230)",
+  "font-weight: 600; font-size: 13px",
+].join(";");
+
 export default function consoleIntro() {
-  console.log(
-    "%c * JSS-01 | JavaScript Software Synthesizer *",
-    "background: rgb(1, 0, 76); color: rgb(230, 230, 230); font-weight: 600; font-size: 12px "
-  );
+  console.log("%c * JSS-01 | JavaScript Software Synthesizer *", styles);
   console.log(
     "Since you are here you might want to check our project at GitHub, have a look at the source code, find bugs, submit issues, create pull requests and become part of out community!\nhttps://github.com/michaelkolesidis/javascript-software-synthesizer"
   );
