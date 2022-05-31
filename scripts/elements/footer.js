341c938b03a8054fc251ecc7346c54014979c0cf
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-05-31 18:28:50 +0300

    added license to footer

--- scripts/elements/footer.js
+++ scripts/elements/footer.js
@@ -7,6 +7,16 @@
 
 export default function footer() {
   return /*html*/ `
-<div><p>© 2022 | Made with ♥ by <a href="https://github.com/michaelkolesidis" target="_blank">Michael Kolesidis</a></p></div><div id="version"><a href="https://github.com/michaelkolesidis/javascript-software-synthesizer/releases" target="_blank">v.0.5.4</a></div>
+<div>
+  <p>
+    © 2022 | Made with ♥ by <a href="https://github.com/michaelkolesidis" target="_blank">Michael Kolesidis</a>.
+    <br>
+    Licensed under the <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU General Public License v3.0</a>.
+  </p>
+</div>
+<div id="version">
+  <a href="https://github.com/michaelkolesidis/javascript-software-synthesizer/releases" target="_blank">v.0.5.4
+  </a>
+</div>
 `;
 }
